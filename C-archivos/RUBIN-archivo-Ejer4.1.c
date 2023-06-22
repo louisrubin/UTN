3953bf03fe7f398fcfc3f21fd56ae9a6ff0cdc7f
Rubin Luis <72027738+louisrubin@users.noreply.github.com>
2023-06-22 08:30:15 -0300

    Update Ejer4.1.c
    ahora el programa imprime por consola los nombres ingresados en el texto (cosa que no hacía)

--- C-archivos/RUBIN-archivo-Ejer4.1.c
+++ C-archivos/RUBIN-archivo-Ejer4.1.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
+#define ARCHIVO "cargarNombres.txt"
 
-/* 4.1: Diseñar un programa que permita generar un archivo de texto 
+/* 4.1: DiseÃ±ar un programa que permita generar un archivo de texto 
 que permita cargar cinco nombres separados por punto. 
 Mostrar el contenido del archivo un nombre abajo del otro. */
 
 int main() {
-	char nombre[50];
-	int x;
 	FILE * punt;		/* declaracion del puntero FILE */
+	char nombres[255];
+	int x;
+
+	punt = fopen(ARCHIVO, "w");	/* crea y abre el archivo llamado "cargarNombres"  */
 	
-	punt = fopen("cargarNombres.txt", "w");	/* crea y abre el archivo llamado "cargarNombres"  */
-	
-	if( ferror(punt) ) {
-		/* verifica si hubo algun error en la apertura del archivo */
+	if( ferror(punt) ) {    /* verifica si hubo algun error en la apertura del archivo */
 		printf("Error al abrir el archivo.\n");
-		return 0;	/* termina el programa */
-	}else printf("Abiendo archivo.\n");
-	
-	for(x=0; x<5; x++) {
-		/* se ingresan los nombres y se escribe en el archivo */
-		printf(" Ingrese un nombre: ");
-		fgets(nombre,50,stdin);		/* obtiene el nombre ingresado */
-		fputs(nombre,punt);	/* escribe en el archivo */
+		return 0;	        /* termina el programa */
 	}
+    	else printf("Abriendo archivo '%s'\n", ARCHIVO);
 	
-	fclose(punt);	/* cierra el archivo. */
-	printf("Archivo cerrado.");
-	
+	printf("\n Ingrese 5 nombres separados por un punto\n");
+	fgets(nombres,255,stdin);		/* almacena la cadena en la variable 'nombres' */
+    	fputs(nombres,punt);	/* escribe en el archivo la cadena ingresada */
+
+    	printf("\n");
+	for(x=0; x<255; x++) { 		/* se imprime la cadena escrita en el archivo */
+		if(nombres[x] == '.' || nombres[x] == ' ') printf("\n");
+        	else printf("%c", nombres[x]);
+	}
+	fclose(punt);	/* cierra el archivo utilizando el puntero. */
+	printf("\nArchivo cerrado.");
 	return 0;
 }
-
