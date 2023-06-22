1ff6d2122e77312771cb02dee10ffbfe3c1fec04
Rubin Luis <72027738+louisrubin@users.noreply.github.com>
2023-06-22 09:07:00 -0300

    Update Ejer4.2.c
    arreglé el programa porque guardaba todo en una lista de estructuras y no en el archivo como tenia que ser.
    ahora si hace lo que tiene que hacer :D

--- C-archivos/RUBIN-archivo-Ejer4.2.c
+++ C-archivos/RUBIN-archivo-Ejer4.2.c
@@ -2,43 +2,42 @@
 #include <conio.h> 
 #include <string.h> 
 #define ARCH "clientes.dat" 
-#define TAM 3
+#define TAM 5
 /*
 Agregar al programa, mostrar el contenido del archivo cargado 
 previamente mediante el  siguiente formato
 */
-
 struct registro { 
 	int cliente; 
 	char nombre[20]; 
 	float saldo; 
 };
-
-void imprDatos(struct registro *reg){
+void imprDatos(struct registro reg){
 	/* metodo para imprimir la info de un registro */
-	printf("\n");
 	printf(" Nro. Cliente: %d\n Nombre: %s\n Saldo: $%.1f \n", 
-			reg->cliente, reg->nombre, reg->saldo);
+			reg.cliente, reg.nombre, reg.saldo);
 }
-	
+
 int main() { 
 	FILE *arch; 
-	int x,p=0;
-	
-	struct registro reg[TAM]; 
-	char seguir='n'; 
-	
-	if ((arch=fopen(ARCH,"wb")) == NULL) 
-		printf(" No se pudo abrir el archivo");
-	
-	while(seguir=='n' && p < TAM){ 
+	int x, p=0;	
+	struct registro reg; 
+	char seguir ='n'; 
+
+	/* abre el archivo en modo ESCRITURA y verifica si se realizo con exito */
+	if ( (arch = fopen(ARCH, "wb") ) == NULL) {
+		printf(" No se pudo abrir el archivo en modo ESCRITURA.");
+		return 1;
+	}
+	while(seguir =='n' && p < TAM){ 
+		/* bloque para escribir clientes en el archivo */
 		printf("\n Cliente %d", p+1); 
-		reg[p].cliente = p+1; 
+		        reg.cliente = p+1; 
 		printf("\n Ingrese el nombre: "); 
-		scanf("%s",&reg[p].nombre); 
+		scanf("%s", &reg.nombre); 
 		printf(" Ingrese el saldo: $"); 
-		scanf("%f",&reg[p].saldo); 
-		fwrite( &reg, sizeof(struct registro), 1, arch); 
+		scanf("%f", &reg.saldo); 
+		fwrite( &reg, sizeof(struct registro), 1, arch); 	/* escribe los datos en el archivo */
 		
 		if(p != TAM-1) {
 			printf("\n Desea terminar s/n: ");
@@ -46,16 +45,22 @@ int main() {
 		}		 
 		p++;
 	}	
-	arch = fopen(ARCH, "rb"); /* apertura de archivo READ/BINARY */
-	fread(&reg, sizeof(reg), p, arch);
+	fclose(arch);       /* cierro el archivo en modo Escritura */
+
+	/* abre el archivo en modo LECTURA y verifica si se realizo con exito */
+	if ( (arch = fopen(ARCH, "rb") ) == NULL) {
+	    /* vuelvo a abrir el archivo pero ahora en modo READ/BINARY */
+		printf(" No se pudo abrir el archivo en modo LECTURA.");
+		return 1;
+	}
 	
-	x=0;
-	while(x < p){	/* imprimir todos los elementos del array */
-		printf("-------------------");
-		imprDatos(&reg[x]);
-		x++;
+	printf("\n");
+	while( fread(&reg, sizeof(reg), 1, arch) == 1 ){	/* aca imprime las cadenas guardado en el archivo */
+		printf("-------------------\n");
+	    	imprDatos(reg);         /* procedimiento para imprimir los datos de un cliente */
 	} printf("-------------------");
 	
-	fclose(arch);
+	fclose(arch);	/* cierro el archivo nuevamente */
 	getch(); 
+	return 0;
 } 
