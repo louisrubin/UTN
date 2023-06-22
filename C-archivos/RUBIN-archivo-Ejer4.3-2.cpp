d5a07edf3153e55e4c7bc108cd37171ddb76572b
Rubin Luis <72027738+louisrubin@users.noreply.github.com>
2023-06-21 23:19:23 -0300

    Add files EJER 5
    subo los 2 archivos del ejercicio 5 del TP de Archivos (Laboratorio)

---
+++ C-archivos/RUBIN-archivo-Ejer4.3-2.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define ARCH "alumnos.bin"
+#define TAM 5
+
+/* PROGRAMA QUE LEE LOS ALUMNOS REGISTRADOS EN EL ARCHIVO 'alumnos.bin' */
+
+struct Alumno {
+	/* estructura general de cualquier alumno */
+	int dni;
+	char apellido[20], nombre[20];
+	float nota;
+};
+
+struct Detalles {
+	/* estructura de todos los detalles finales */
+	int cantAlumns=0, cantAlumAprob=0, cantAlumDesap=0;
+	float promNotaAlumn;
+	float porcAlumAprob, porcAlumDesap;
+	struct Alumno alumnNotaAlta, alumnNotaBaja;
+} details;
+
+void sumarInfo(struct Alumno alumn){
+	/* metodo que se encarga cargar infos a partir de un alumno */
+	
+	if(alumn.nota > details.alumnNotaAlta.nota){
+		details.alumnNotaAlta = alumn;
+	}
+	if(alumn.nota < details.alumnNotaBaja.nota){
+		details.alumnNotaBaja = alumn;
+	}
+	if (alumn.nota >= 6) details.cantAlumAprob++;
+	if (alumn.nota < 6) details.cantAlumDesap++;
+}
+
+void imprTodaInfo(){
+	/* metodo para imprimir toda la info final */
+	printf("\n TOTAL ALUMNOS: %d", details.cantAlumns);
+	printf("\n ALUMNO CON MAYOR NOTA: [ %d, '%s %s', (%.1f) ] ",
+		   details.alumnNotaAlta.dni,
+		   details.alumnNotaAlta.nombre,
+		   details.alumnNotaAlta.apellido,
+		   details.alumnNotaAlta.nota
+		   );	
+	printf("\n ALUMNO CON MENOR NOTA: [ %d, '%s %s', (%.1f) ] ",
+		   details.alumnNotaBaja.dni,
+		   details.alumnNotaBaja.nombre,
+		   details.alumnNotaBaja.apellido,
+		   details.alumnNotaBaja.nota
+		   );
+	printf("\n PROMEDIO NOTA DE ALUMNOS: %.1f", details.promNotaAlumn);
+	printf("\n CANTIDAD DE ALUMNOS APROBADOS: %d",details.cantAlumAprob);
+	printf("\n CANTIDAD DE ALUMNOS DESAPROBADOS: %d", details.cantAlumDesap);
+	printf("\n % DE ALUMNOS APROBADOS: %.1f%", details.porcAlumAprob);
+	printf("\n % DE ALUMNOS DESAPROBADOS: %.1f%", details.porcAlumDesap);
+}
+	
+int main() {
+	FILE *arch;
+	struct Alumno alumns;
+	float sumaNota=0;
+	details.alumnNotaAlta.nota = 0;	/* asignando valor de partida */
+	details.alumnNotaBaja.nota = 10.1;	/* para compararlo después */
+	
+	arch = fopen(ARCH, "rb");
+	
+	if ( arch == NULL ){
+		printf(" No se pudo abrir el archivo %s\n",ARCH);
+		return 1;
+	}
+	
+	printf("-----------------------------------------------------------------\n");
+	printf(" DNI\t\tNOMBRE\t\tAPELLIDO\t\tNOTA\n");
+	printf("-----------------------------------------------------------------\n");
+	
+	while( fread( &alumns, sizeof(alumns), 1, arch ) == 1 ){
+		printf(" %d\t%-15s\t%-15s\t\t%.1f\n", alumns.dni, alumns.nombre, alumns.apellido, alumns.nota);
+		sumaNota += alumns.nota;
+		
+		sumarInfo(alumns);
+		
+		details.cantAlumns++;	/* aumenta la cant de alums del STRUCT 'DETALLES' */
+	}
+	printf("-----------------------------------------------------------------\n");
+	
+	fclose(arch);
+	
+	/* obtiene los promedios y porcentajes */
+	details.promNotaAlumn = sumaNota / details.cantAlumns;
+	details.porcAlumAprob = (100 * details.cantAlumAprob ) / details.cantAlumns;
+	details.porcAlumDesap = (100 * details.cantAlumDesap ) / details.cantAlumns;
+	
+	imprTodaInfo();
+	
+	return 0;
+}
+
---
+++ C-archivos/RUBIN-archivo-Ejer4.3.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define ARCH "alumnos.bin"
+#define TAM 5
+
+/* PROGRAMA DE CARGA DE ALUMNOS EN EL ARCHIVO 'alumnos.bin' */
+
+struct Alumno {
+	/* estructura general de cualquier alumno */
+	int dni;
+	char apellido[20], nombre[20];
+	float nota;
+};
+
+int main() {
+	FILE *arch;
+	struct Alumno alumns;
+
+	arch = fopen(ARCH, "wb");
+	int p=0;
+	char seg = 'n';	
+	
+	do {
+		printf("\n Ingrese DNI: ");
+		scanf("%d",&alumns.dni); 
+		printf(" Ingrese nombre: "); 
+		scanf("%s",&alumns.nombre); 
+		printf(" Ingrese apellido: "); 
+		scanf("%s",&alumns.apellido); 
+		printf(" Ingrese nota: ");
+		scanf("%f",&alumns.nota); 
+		fwrite( &alumns, sizeof(alumns), 1, arch); /* function para escribir en el archivo */
+		
+		p++;		
+		if(p < TAM-1) {
+			printf("\n Desea terminar s/n: ");
+			scanf("\n%c",&seg);
+		}
+	}while(seg=='n' && p < TAM);
+	
+	fclose(arch);
+	return 0;
+}
