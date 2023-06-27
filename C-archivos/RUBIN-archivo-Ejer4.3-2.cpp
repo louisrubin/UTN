3b23cf6b0c97d64f3efeb3791221b96a5c8a38d6
Rubin Luis <72027738+louisrubin@users.noreply.github.com>
2023-06-27 19:31:57 -0300

    Fix Ejer4.3-2
    correji unos errores que impedía correr este programa

--- C-archivos/RUBIN-archivo-Ejer4.3-2.cpp
+++ C-archivos/RUBIN-archivo-Ejer4.3-2.cpp
@@ -15,7 +15,7 @@ struct Alumno {
 
 struct Detalles {
 	/* estructura de todos los detalles finales */
-	int cantAlumns=0, cantAlumAprob=0, cantAlumDesap=0;
+	int cantAlumns, cantAlumAprob, cantAlumDesap;
 	float promNotaAlumn;
 	float porcAlumAprob, porcAlumDesap;
 	struct Alumno alumnNotaAlta, alumnNotaBaja;
@@ -38,61 +38,62 @@ void imprTodaInfo(){
 	/* metodo para imprimir toda la info final */
 	printf("\n TOTAL ALUMNOS: %d", details.cantAlumns);
 	printf("\n ALUMNO CON MAYOR NOTA: [ %d, '%s %s', (%.1f) ] ",
-		   details.alumnNotaAlta.dni,
-		   details.alumnNotaAlta.nombre,
-		   details.alumnNotaAlta.apellido,
-		   details.alumnNotaAlta.nota
-		   );	
+			details.alumnNotaAlta.dni,
+			details.alumnNotaAlta.nombre,
+			details.alumnNotaAlta.apellido,
+			details.alumnNotaAlta.nota
+		);	
 	printf("\n ALUMNO CON MENOR NOTA: [ %d, '%s %s', (%.1f) ] ",
-		   details.alumnNotaBaja.dni,
-		   details.alumnNotaBaja.nombre,
-		   details.alumnNotaBaja.apellido,
-		   details.alumnNotaBaja.nota
-		   );
+			details.alumnNotaBaja.dni,
+			details.alumnNotaBaja.nombre,
+			   details.alumnNotaBaja.apellido,
+			   details.alumnNotaBaja.nota
+		);
 	printf("\n PROMEDIO NOTA DE ALUMNOS: %.1f", details.promNotaAlumn);
 	printf("\n CANTIDAD DE ALUMNOS APROBADOS: %d",details.cantAlumAprob);
 	printf("\n CANTIDAD DE ALUMNOS DESAPROBADOS: %d", details.cantAlumDesap);
-	printf("\n % DE ALUMNOS APROBADOS: %.1f%", details.porcAlumAprob);
-	printf("\n % DE ALUMNOS DESAPROBADOS: %.1f%", details.porcAlumDesap);
+	printf("\n PORC. ALUMNOS APROBADOS: %.0f%%", details.porcAlumAprob);
+	printf("\n PORC. ALUMNOS DESAPROBADOS: %.0f%%", details.porcAlumDesap);
 }
-	
+		
 int main() {
 	FILE *arch;
 	struct Alumno alumns;
 	float sumaNota=0;
-	details.alumnNotaAlta.nota = 0;	/* asignando valor de partida */
-	details.alumnNotaBaja.nota = 10.1;	/* para compararlo después */
 	
+	details.cantAlumns=0; 			/* asignando valores iniciales */
+	details.cantAlumAprob=0; 		/* para compararlo despuÃ©s */
+	details.cantAlumDesap=0;
+	details.alumnNotaAlta.nota = 0;	
+	details.alumnNotaBaja.nota = 10.1;	
+
 	arch = fopen(ARCH, "rb");
-	
+
 	if ( arch == NULL ){
 		printf(" No se pudo abrir el archivo %s\n",ARCH);
 		return 1;
 	}
-	
 	printf("-----------------------------------------------------------------\n");
 	printf(" DNI\t\tNOMBRE\t\tAPELLIDO\t\tNOTA\n");
 	printf("-----------------------------------------------------------------\n");
-	
+
 	while( fread( &alumns, sizeof(alumns), 1, arch ) == 1 ){
 		printf(" %d\t%-15s\t%-15s\t\t%.1f\n", alumns.dni, alumns.nombre, alumns.apellido, alumns.nota);
 		sumaNota += alumns.nota;
-		
+
 		sumarInfo(alumns);
-		
+
 		details.cantAlumns++;	/* aumenta la cant de alums del STRUCT 'DETALLES' */
 	}
 	printf("-----------------------------------------------------------------\n");
-	
-	fclose(arch);
+
+	fclose(arch);	/* cierro el archivo */
 	
 	/* obtiene los promedios y porcentajes */
 	details.promNotaAlumn = sumaNota / details.cantAlumns;
 	details.porcAlumAprob = (100 * details.cantAlumAprob ) / details.cantAlumns;
 	details.porcAlumDesap = (100 * details.cantAlumDesap ) / details.cantAlumns;
 	
-	imprTodaInfo();
-	
+	imprTodaInfo();	/* procedimiento para imprimir toda la info detallada*/
 	return 0;
 }
-
